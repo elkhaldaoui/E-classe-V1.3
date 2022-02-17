ff52c9111f95f675301a3e65d51812b3593d7b1f
Oussama Elkhaldaoui <93930380+elkhaldaoui@users.noreply.github.com>
2022-02-17 11:19:29 +0100

    backup

--- deletec.php
+++
@@ -1,8 +0,0 @@
-<?php
-$connection = mysqli_connect ("localhost","root","","e_classe_db");
-$queery = "SELECT * FROM course ";
-$results = mysqli_query($connection,$queery);
-$id = $_GET['id'];
-mysqli_query($connection, "DELETE FROM course WHERE id=$id");
-$_SESSION['message'] = "Address deleted!"; 
-echo "<script>window.location.href = 'course.php';</script>";
